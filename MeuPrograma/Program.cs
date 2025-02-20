4dbc325cadf80d1c0f6efbf345df8c2f720a1081
RicardoMota22 <ricardomota.artist@gmail.com>
2025-02-20 23:16:28 +0000

    Add comments

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// 1st comment 
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// 2nd comment 
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
