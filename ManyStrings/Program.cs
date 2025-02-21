8492f64a68ad0dfd0a8d16f851a7aa6138397b7a
RicardoMota22 <ricardomota.artist@gmail.com>
2025-02-21 10:42:23 +0000

    Concatenations and Interpolations

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -11,12 +11,31 @@ namespace ManyStrings
             string message2 = "\u0027"; //Plica
             string s = "Um tab\t e um copyright\t\u00A9"; //copyright unicode 
             //with string
-            
+
+
+            //Exercise 5 
+            int z = 5;
+            float y = 5.1f;
+
+            //Concatenation
+            string f = "Connected"+ " String";
+            string a = "String " + 2;
+            string b = "String " + z;
+
+            //Interpolation
+
+            string sn = $"{z} mais {y} e' igual a {z + y}";
             
             Console.WriteLine("Hello LP1!");
             Console.WriteLine(message);
             Console.WriteLine(message2);
             Console.WriteLine(s);
+            Console.WriteLine(f);
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            Console.WriteLine(sn);
         }
     }
 }
