42388bc98e6150b33ea1d67e1ef0d9f941657c70
RicardoMota22 <ricardomota.artist@gmail.com>
2025-02-21 16:30:45 +0000

    Format variables in different ways

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 
+
 namespace ManyStrings
 {
     public class Program
     {
+        
         private static void Main(string[] args)
         {
+            //This changed the question mark that was printed on the money 
+            //script to a euro sign 
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
             //strings are types of references, not value like int,float,double
             string message = "Hello World";
             string message2 = "\u0027"; //Plica
@@ -25,6 +31,26 @@ namespace ManyStrings
             //Interpolation
 
             string sn = $"{z} mais {y} e' igual a {z + y}";
+
+            //Exercise 6 
+            double xx = 1.23456; //if you change 4 to 5 it rounds to 1.24
+            int ii = 19;
+
+            //only 2 decimal points 
+            string xxk = $"xx={xx:f2}"; //Answer is 1.23
+
+            //Percentage, only 1 decimal point
+            
+            //tests
+            //string x2 = $".{10,4:x}. e .{11,-4:x}.";
+
+            //string s3 = $"'{5,-6:f2}' e '{0.2:p1}'";
+
+            string xx2 = $"'{xx:p1}'"; //Calculates percentage of double xx
+            string ii2 = $"'{ii:x}'"; //Calculates Hexadecimal
+             
+            string ii3 = $"'{ii:C}'"; //Calculates Moeda
+
             
             Console.WriteLine("Hello LP1!");
             Console.WriteLine(message);
@@ -36,6 +62,14 @@ namespace ManyStrings
             Console.WriteLine(b);
 
             Console.WriteLine(sn);
+
+            Console.WriteLine(xxk);
+            //Console.WriteLine(x2);
+            //Console.WriteLine(s3);
+            Console.WriteLine(xx2);
+
+            Console.WriteLine(ii2);
+            Console.WriteLine(ii3);
         }
     }
 }
