1abae6f0714cf562489e908d106cdf39f833215a
RicardoMota22 <ricardomota.artist@gmail.com>
2025-02-21 17:15:35 +0000

    Last exercise and some of project Smiley

---
+++ AskForInput/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            Console.WriteLine("Insert whole number: ");
+            string str = Console.ReadLine();//Asks for an input from user
+
+            //Converts string to integer 
+            int i = int.Parse(str ); 
+
+            
+            //Console.WriteLine(str);
+            Console.WriteLine(i);
+
+
+
+            Console.WriteLine("Insert real number: ");
+            string str2 = Console.ReadLine();//Asks for an input from user
+        }
+    }
+}
---
+++ Smiley/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace Smiley
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Insere codigo  : ");
+            string str = Console.ReadLine();//Asks for an input from user
+
+            //Print of Unicode
+            char copyrightSymbol = '\u00A9';
+
+            Console.WriteLine(str);
+            Console.WriteLine(copyrightSymbol);
+        }
+    }
+}
