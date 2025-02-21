c05b677f916511a867f735c1fb562f7e0ae5539d
RicardoMota22 <ricardomota.artist@gmail.com>
2025-02-21 09:45:22 +0000

    Whole Variables

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+
+            // Declarar variavel
+            //int numero;
+            
+            //Atribuir valor a variavel
+            //numero = 8;
+            
+            // Declarar e attribuir valor na mesma linha 
+            int numero = 8;
+
+            // Declarar e attribuir valor na mesma linha 
+            float pi = 3.1415f;
+
+
+
+            //Mostrar Valor no ecra 
+            Console.WriteLine(numero);
+
+            Console.WriteLine(pi);
+        
+        }
+
+    }
+}
